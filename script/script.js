cc0c609ab02834f2b96391b74df00419ff46833a
Isaabellaasmit <isabellasmit.2604@gmail.com>
2023-01-18 14:59:37 +0100

    second commit

--- script/script.js
+++ script/script.js
@@ -6,12 +6,12 @@ let voorBerg = document.getElementById("bergen_front");
 let achterBerg = document.getElementById("bergen_back");
 let text = document.getElementById("text");
 
-window.addEventListener("scroll", function(){
-    let value = window.scrollY;
-    stars.style.left = value * 0.25 + "px"; 
-    moon.style.top = value * 1.05 + "px";
-    voorBerg.style.top = value * 0 + "px";
-    achterBerg.style.top = value * 0.5 + "px";
-    text.style.marginRight = value * 4 + "px";
-    text.style.marginTop = value * 1.5 + "px";
-})
+// window.addEventListener("scroll", function(){
+//     let value = window.scrollY;
+//     stars.style.left = value * 0.25 + "px"; 
+//     moon.style.top = value * 1.05 + "px";
+//     voorBerg.style.top = value * 0 + "px";
+//     achterBerg.style.top = value * 0.5 + "px";
+//     text.style.marginRight = value * 4 + "px";
+//     text.style.marginTop = value * 1.5 + "px";
+// })
