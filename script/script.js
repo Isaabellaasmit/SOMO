178c070594db16b9fee11b5dd7c16ef47c91fa00
Isaabellaasmit <isabellasmit.2604@gmail.com>
2023-01-18 14:47:22 +0100

    first commit

---
+++ script/script.js
@@ -0,0 +1,17 @@
+console.log("test")
+
+let moon = document.getElementById("maan");
+let stars = document.getElementById("sterren");
+let voorBerg = document.getElementById("bergen_front");
+let achterBerg = document.getElementById("bergen_back");
+let text = document.getElementById("text");
+
+window.addEventListener("scroll", function(){
+    let value = window.scrollY;
+    stars.style.left = value * 0.25 + "px"; 
+    moon.style.top = value * 1.05 + "px";
+    voorBerg.style.top = value * 0 + "px";
+    achterBerg.style.top = value * 0.5 + "px";
+    text.style.marginRight = value * 4 + "px";
+    text.style.marginTop = value * 1.5 + "px";
+})
