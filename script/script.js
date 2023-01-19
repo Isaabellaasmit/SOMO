81395bc3515050e4dff4103673924fa6fa9c6524
Isaabellaasmit <isabellasmit.2604@gmail.com>
2023-01-19 17:46:39 +0100

    animatie in de maak

--- script/script.js
+++ script/script.js
@@ -1,10 +1,48 @@
 console.log("test")
 
-let moon = document.getElementById("maan");
-let stars = document.getElementById("sterren");
-let voorBerg = document.getElementById("bergen_front");
-let achterBerg = document.getElementById("bergen_back");
-let text = document.getElementById("text");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import scrollama from "scrollama";
+
+// instantiate the scrollama
+// const scroller = scrollama();
+
+// // setup the instance, pass callback functions
+// scroller
+//   .setup({
+//     step: ".step",
+//   })
+//   .onStepEnter((response) => {
+//     // { element, index, direction }
+//   })
+//   .onStepExit((response) => {
+//     // { element, index, direction }
+//   });
+
+
+
+
+
+
+// let moon = document.getElementById("maan");
+// let stars = document.getElementById("sterren");
+// let voorBerg = document.getElementById("bergen_front");
+// let achterBerg = document.getElementById("bergen_back");
+// let text = document.getElementById("text");
 
 // window.addEventListener("scroll", function(){
 //     let value = window.scrollY;
